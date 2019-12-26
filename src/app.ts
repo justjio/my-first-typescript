2af8e220cf46e2fb40df440a31b4fafd59cfd22f
Joseph Obiagba <j.i.obiagba2016@gmail.com>
2019-12-27 00:04:08 +0100

    Getting ready for quick production -  cloud MongoDB in use now!

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,14 @@
 import * as express from "express"
 import { Routes } from "./routes/all-routes" // Ensure this has been created first
 import * as mongoose from "mongoose"
+import * as helmet from "helmet"
+import * as compression from "compression"
 
 //Create the App class
 class App {
     public app: express.Application
-    public mongoUrl: string = "mongodb://localhost/contact"
+    public mongoLocalUrl: string = "mongodb://localhost/contact"
+    public mongoCloudUrl: string = "mongodb+srv://jiobiagbaDB:obidudu9293@cluster0-ywgnl.azure.mongodb.net/typescript?retryWrites=true&w=majority"
     public router: Routes = new Routes() //Ensure this has been set up in routes folder
 
     constructor() {
@@ -19,11 +22,13 @@ class App {
     private config(): void {
         this.app.use(express.json()) //This will ensure support for post data in json format
         this.app.use(express.urlencoded({ extended: false })) //This will ensure support for x-www-form-urlencoded post data
+        this.app.use(compression())
+        this.app.use(helmet())
     }
 
     private mongoSetup(): void {
         // mongoose.Promise = global.Promise
-        mongoose.connect(this.mongoUrl, {
+        mongoose.connect(this.mongoCloudUrl, {
             useNewUrlParser: true,
             useUnifiedTopology: true
         })
